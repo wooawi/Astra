a9451ee1af97a23d611cea5e071e43286462a032
wooawi <dinara5.zinatulina@gmail.com>
2026-06-18 14:08:06 +0700

    ФИНАЛ

--- app/src/main/java/com/example/astra/MoonUtils.kt
+++ app/src/main/java/com/example/astra/MoonUtils.kt
@@ -1,25 +1,13 @@
 package com.example.astra
 
-fun getMoonImage(phase: String): Int {
-
-    return when (phase) {
-
-        "New Moon" -> R.drawable.new_moon
-
-        "Waxing Crescent" -> R.drawable.waxing_crescent
-
-        "First Quarter" -> R.drawable.first_quarter
-
-        "Waxing Gibbous" -> R.drawable.waxing_gibbous
-
-        "Full Moon" -> R.drawable.full_moon
-
-        "Waning Gibbous" -> R.drawable.waning_gibbous
-
-        "Last Quarter" -> R.drawable.last_quarter
-
-        "Waning Crescent" -> R.drawable.waning_crescent
-
-        else -> R.drawable.full_moon
-    }
+fun getMoonImage(phase: String): Int = when (phase) {
+    "Новолуние" -> R.drawable.new_moon
+    "Растущий серп" -> R.drawable.waxing_crescent
+    "Первая четверть" -> R.drawable.first_quarter
+    "Растущая Луна" -> R.drawable.waxing_gibbous
+    "Полнолуние" -> R.drawable.full_moon
+    "Убывающая Луна" -> R.drawable.waning_gibbous
+    "Последняя четверть" -> R.drawable.last_quarter
+    "Стареющий серп" -> R.drawable.waning_crescent
+    else -> R.drawable.full_moon
 }
